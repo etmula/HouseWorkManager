e3873b61680b37d7880206538207e691009208ea
etmula <mountainroad@hotmail.co.jp>
2020-04-18 16:17:10 +0900

    add acconts functions

--- houseworkmanager/houseworkmanager/settings.py
+++ houseworkmanager/houseworkmanager/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'houseworkmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = '/code/static'
+
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'exec:index'
+
+AUTH_USER_MODEL = 'accounts.User'
