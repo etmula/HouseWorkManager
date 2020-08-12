12eda8b41bd223391418f39138135fdc4a898d30
etmula <mountainroad@hotmail.co.jp>
2020-08-12 13:28:16 +0900

    Update History

--- houseworkmanager/history/models.py
+++ houseworkmanager/history/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 from django.utils.timezone import localdate
-
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Recode(models.Model):
@@ -12,6 +12,11 @@ class Recode(models.Model):
     category = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     point = models.IntegerField()
+    group = models.ForeignKey('accounts.Group', on_delete=models.CASCADE, related_name='recodes')
 
     def __str__(self):
         return f'{self.date} {self.executer.username} {self.name}'
+
+    def get_absolute_url(self):
+        return reverse("history:recode_detail", kwargs={"pk": self.pk})
+    
--- houseworkmanager/history/views.py
+++ houseworkmanager/history/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, DeleteView, ListView, UpdateView
+from django.utils import timezone
 
 from .models import Recode
+from stats.table_generator import score_user_line
+
+def RecodeCancel(request):
+    request.POST.get()
+    return reverse_lazy('history:recode_list')
 
 
 class RecodeCreateView(CreateView):
@@ -31,5 +37,27 @@ class RecodeDeleteView(DeleteView):
 class RecodeListView(ListView):
     model = Recode
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context['data'] = score_user_line(self.request.user.group.users.all(), now.year, now.month)
+        return context
+
     def get_queryset(self):
         return Recode.objects.filter(executer__in=self.request.user.group.users.all())
+
+
+class RecodeListMonthlyView(ListView):
+    model = Recode
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = score_user_line(self.request.user.group.users.all(), self.kwargs.get('year'), self.kwargs.get('month'))
+        return context
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        query_set.filter(date__year=self.kwargs.get('year'), date__month=self.kwargs.get('month'))
+        return query_set
+
+    
