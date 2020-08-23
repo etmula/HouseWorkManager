401905ec6f8686fc57535047afa40399ac19c74f
etmula <mountainroad@hotmail.co.jp>
2020-08-23 20:54:24 +0900

    Remove Recode(Point,Count)TableMonthlyView

--- houseworkmanager/history/urls.py
+++ houseworkmanager/history/urls.py
@@ -5,10 +5,8 @@ from . import views
 app_name = 'history'
 urlpatterns = [
     path('recode/', views.RecodeListView.as_view(), name='recode_list'),
-    path('recode/<int:pk>', views.RecodeDetailView.as_view(), name='recode_detail'),
+    path('report/<int:year>/<int:month>', views.RecodeListMonthlyView.as_view(), name='recode_list_monthly'),
+    path('recode/detail/<int:pk>', views.RecodeDetailView.as_view(), name='recode_detail'),
     path('recode/create', views.RecodeCreateView.as_view(), name='recode_create'),
     path('recode/delete/<int:pk>', views.RecodeDeleteView.as_view(), name='recode_delete'),
-    path('report/<int:year>/<int:month>', views.RecodeListMonthlyView.as_view(), name='recode_list_monthly'),
-    path('report/point/<int:year>/<int:month>', views.RecodePointTableMonthlyView.as_view(), name='report_point_monthly'),
-    path('report/count/<int:year>/<int:month>', views.RecodeCountTableMonthlyView.as_view(), name='report_count_monthly'),
 ]
