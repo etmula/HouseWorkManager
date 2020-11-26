76fe8081653f04f5d82d2812071590482dd56d6a
etmula <mountainroad@hotmail.co.jp>
2020-11-26 17:31:42 +0900

    Update urls.py

--- houseworkmanager/exec/urls.py
+++ houseworkmanager/exec/urls.py
@@ -6,7 +6,4 @@ app_name = 'exec'
 urlpatterns = [
     path('home', views.HomeView.as_view(), name='home'),
     path('exec', views.ExecView.as_view(), name='exec'),
-    path('history', views.HistoryView.as_view(), name='history'),
-    path('work', views.WorkView.as_view(), name='work'),
-    path('category', views.CategoryView.as_view(), name='category'),
 ]
