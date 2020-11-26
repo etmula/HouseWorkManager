8c4a16dd50420e72d24357f722329687fe2188ee
etmula <mountainroad@hotmail.co.jp>
2020-11-26 22:03:16 +0900

    Update wsgi.py

--- houseworkmanager/houseworkmanager/wsgi.py
+++ houseworkmanager/houseworkmanager/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'houseworkmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'houseworkmanager.houseworkmanager.settings')
 
 application = get_wsgi_application()
