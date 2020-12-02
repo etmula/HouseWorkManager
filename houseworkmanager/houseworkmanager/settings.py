fef04d49eb4ed2a77975d7fae656110efedfd61b
etmula <mountainroad@hotmail.co.jp>
2020-12-03 01:29:04 +0900

    Update DEBUG

--- houseworkmanager/houseworkmanager/settings.py
+++ houseworkmanager/houseworkmanager/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-DEBUG = False
+DEBUG = True
 
 
 # Quick-start development settings - unsuitable for production
